0d7c57b95f6933c86c4a12d8f747083edf84f0ac
theRayDev <69957239+theRayDev@users.noreply.github.com>
2025-07-27 22:13:03 +0530

    full example of difference between NumPy and python sequence

--- numpy01.py
+++ numpy01.py
@@ -1,9 +1,16 @@
-# example 1
-
+# example 1 - with average spped of 20ms
 a = [1,2,3]
 b = [4,5,6]
-c = []
-for i in range(0,len(a)):
-    c.append(a[i]*b[i])
-print(c)
+#c = []
+#for i in range(0,len(a)):
+#    c.append(a[i]*b[i])
+#print(c)
 
+# example 1 (using NumPy)
+import numpy as np
+array1 = np.array(a)
+array2 = np.array(b)
+
+c = array1*array2
+
+print(c)
